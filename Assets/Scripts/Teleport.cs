3272d9ab8b3af948368b08336a69144b82e138e9
ShadeLorde <batkinson3@5142N-02.butler.butlercc.edu>
2015-11-11 15:38:27 -0600

    Changed stuff
    Added mountains and other things

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Teleport : MonoBehaviour {
+
+	public GameObject destination;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider player) {
+		player.transform.position = destination.transform.position;
+	}
+}
---
+++ Assets/Scripts/restart.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+
+public class restart : MonoBehaviour {
+	
+	public GameObject destination;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider player) {
+		Application.LoadLevel (0);
+	}
+}
